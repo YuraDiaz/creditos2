e4c905ed3225c2417866cfad68d4c15d6aa4a411
leidytatiana02 <leidytatianasarriar@gmail.com>
2022-05-28 21:12:46 -0600

    videos

--- view/informacion.php
+++ view/informacion.php
@@ -21,16 +21,66 @@
                             <tr class="mx-6">
                                 <th><img src="../images/5.jpg" class="rounded float-left" alt="Imagen" width="450" height="400"></th>
                                 <td class="col-sm-7">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h5 class="card-title"><b> Pueblo Misak - Guambiano </b></h5>
+                                    <div class="accordion" id="accordionExample">
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingOne">
+                                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    UBICACIÓN GEOGRÁFICA
+                                                </button>
+                                            </h2>
+                                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4WgdHyte-CE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div class="card-body">
-                                            <a href="#" class="btn btn-primary">UBICACIÓN GEOGRÁFICA</a>
-                                            <a href="#" class="btn btn-primary">POBLACIÓN</a>
-                                            <a href="#" class="btn btn-primary">LENGUA</a>
-                                            <a href="#" class="btn btn-primary">CULTURA E HISTORIA</a>
-                                            <a href="#" class="btn btn-primary">ECONOMÍA</a>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingTwo">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                    CULTURA E HISTORIA
+                                                </button>
+                                            </h2>
+                                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/CXRZCwWtZPM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingThree">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                    POBLACIÓN
+                                                </button>
+                                            </h2>
+                                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/_1oOOZKijqw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingThree">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#cuatro" aria-expanded="false" aria-controls="cuatro">
+                                                    LENGUA
+                                                </button>
+                                            </h2>
+                                            <div id="cuatro" class="accordion-collapse collapse" aria-labelledby="cuatro" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/cB6wMLLlZTg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="accordion-item">
+                                            <h2 class="accordion-header" id="headingThree">
+                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#cinco" aria-expanded="false" aria-controls="cinco">
+                                                    ECONOMÍA
+                                                </button>
+                                            </h2>
+                                            <div id="cinco" class="accordion-collapse collapse" aria-labelledby="cinco" data-bs-parent="#accordionExample">
+                                                <div class="accordion-body">
+                                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/SwM_HrXWSME" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </td>
